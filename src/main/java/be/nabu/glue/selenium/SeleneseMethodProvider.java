4b303b6d567db92ffbf75ac4616ea49be429747d
alex <alex@nabu.be>
2014-12-23 15:14:16 +0100

    Fix parsing exception

--- src/main/java/be/nabu/glue/selenium/SeleneseMethodProvider.java
+++ src/main/java/be/nabu/glue/selenium/SeleneseMethodProvider.java
@@ -346,7 +346,7 @@ public class SeleneseMethodProvider implements MethodProvider {
 					else if (step.getAction().equalsIgnoreCase("clickAndWait")) {
 						element.click();
 						try {
-							Thread.sleep(step.getContent() == null ? this.sleep : new Long(step.getContent()));
+							Thread.sleep(step.getContent() == null || step.getContent().isEmpty() ? this.sleep : new Long(step.getContent()));
 						}
 						catch (InterruptedException e) {
 							// continue
